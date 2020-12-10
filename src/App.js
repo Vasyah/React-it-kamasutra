829c42bab342ded8f4fa4ec2a1253564a823aa2f
Vasyah <Souji_933@mail.ru>
2020-12-10 14:38:44 +0700

    #12 - divide on components & normalize add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-wrp">
+      <Header/>
+
+      <aside className="sidebar">
+        <nav>
+          <div>Profile</div>
+          <div>Profile</div>
+          <div>Profile</div>
+          <div>Profile</div>
+          <div>Profile</div>
+        </nav>
+      </aside>
+
+      <main className="content">
+        <div className="content__logo-wrp"><img src="https://s1.1zoom.ru/b5050/984/Canada_Parks_Lake_Mountains_Forests_Scenery_Rocky_567540_1920x1200.jpg" alt="" /></div>
+        <article>
+          <h3 className="title">Title</h3>
+
+        </article>
+      </main>
     </div>
   );
 }
 
+
 export default App;
+
---
+++ src/Header.jsx
@@ -0,0 +1,15 @@
+import './Header.css';
+
+const Header = () => {
+  return (
+      <header className="header">
+        <div className="header__logo-wrp"><img src="https://free-images.com/or/617d/18_icon_b_hungary.jpg" alt="logo" /></div>
+        <h3 className="title">TITLE</h3>
+      </header>
+
+  );
+}
+
+
+export default Header;
+
